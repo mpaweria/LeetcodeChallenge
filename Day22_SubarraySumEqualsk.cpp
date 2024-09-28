21d749c8734aeb9423ebb6ce032c97a10cdb44f6
Maitreyi Paweria <maitreyipaweria@Maitreyis-MacBook-Air.local>
2024-09-28 20:12:42 +0530

    LC Day 22

--- Day22_SubarraySumEqualsk.cpp
+++ Day22_SubarraySumEqualsk.cpp
@@ -24,3 +24,27 @@ public:
         return ans;
     }
 };
+
+
+// Approach 2
+
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        unordered_map<int,int> map;
+
+        int ans = 0, currSum = 0;
+        map[0] = 1;
+
+        for(int i=0; i<n; i++) {
+            currSum += arr[i];
+
+            if(map.find(currSum - k) != map.end())
+                ans += map[currSum - k];
+
+            map[currSum]++;
+        }
+        return ans;
+    }
+};
